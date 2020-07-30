f29e645d28663728f8fe53444cdd29532152617e
Russ Savage <russ@influxdata.com>
2020-07-29 21:44:23 -0700

    feat(install): everything dynamic, config profile support

--- install.sh
+++ install.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
-ENV_FILE=.env
-if test -f "$ENV_FILE"; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
 export PATH=$GOPATH/src/github.com/influxdata/influxdb/bin/darwin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 
-if [ -n "$BUCKETS_STACK_ID" ]; then
-  echo "found stack ids" 
-  #INFLUX_TOKEN=$INFLUX_TOKEN INFLUX_ORG=$INFLUX_ORG telegraf --config $TELEGRAF_URL
+if [ -z "$1" ]; then
+    echo "No env supplied, assuming local (default)"
+    echo "Checking if this instance has been set up..."
+    IS_SETUP=$(influx config ls --json)
+    if [ -z "$IS_SETUP" ]; then
+        influx setup -f -b telegraf -o influxdata -u russ -p something
+    fi
 else
-  echo "Can't find any Stack Ids, so let's set things up."
-  influx setup -f -b telegraf -o influxdata -u russ -p something
-  BUCKETS_STACK_ID=$(influx stacks init -n buckets --json | jq -r '.ID')
-  TELEGRAFS_STACK_ID=$(influx stacks init -n telegrafs --json | jq -r '.ID')
-  DASHBOARDS_STACK_ID=$(influx stacks init -n dashboards --json | jq -r '.ID')
-  TASKS_STACK_ID=$(influx stacks init -n tasks --json | jq -r '.ID')
-  
-  # Save the stack ids to use later
-  echo "export BUCKETS_STACK_ID=$BUCKETS_STACK_ID" >> .env
-  echo "export TELEGRAFS_STACK_ID=$TELEGRAFS_STACK_ID" >> .env
-  echo "export DASHBOARDS_STACK_ID=$DASHBOARDS_STACK_ID" >> .env
-  echo "export TASKS_STACK_ID=$TASKS_STACK_ID" >> .env
+    ENV_EXISTS=$(influx config set -n $1 -a | grep Error)
+    if [ -z "$ENV_EXISTS" ]; then
+        echo "Successfully set config profile to $1..."
+    else
+        echo "Config profile $1 not found. Exiting."
+        exit 1
+    fi
+fi
+
+echo "Checking for existing Stacks..."
+BUCKETS_STACK_ID=$(influx stacks --stack-name buckets --json | jq -r '.[].ID')
+if [ -z "$BUCKETS_STACK_ID" ]; then
+    BUCKETS_STACK_ID=$(influx stacks init -n buckets --json | jq -r '.ID')
+fi
+
+TELEGRAFS_STACK_ID=$(influx stacks --stack-name telegrafs --json | jq -r '.[].ID')
+if [ -z "$TELEGRAFS_STACK_ID" ]; then
+    TELEGRAFS_STACK_ID=$(influx stacks init -n telegrafs --json | jq -r '.ID')
+fi
+
+DASHBOARDS_STACK_ID=$(influx stacks --stack-name dashboards --json | jq -r '.[].ID')
+if [ -z "$DASHBOARDS_STACK_ID" ]; then
+    DASHBOARDS_STACK_ID=$(influx stacks init -n dashboards --json | jq -r '.ID')
+fi
+
+TASKS_STACK_ID=$(influx stacks --stack-name tasks --json | jq -r '.[].ID')
+if [ -z "$TASKS_STACK_ID" ]; then
+    TASKS_STACK_ID=$(influx stacks init -n tasks --json | jq -r '.ID')
 fi
 
 influx stacks update --stack-id $BUCKETS_STACK_ID -n buckets --template-url file://$(pwd)/buckets.yml
@@ -51,9 +66,3 @@ do
 done
 eval "influx stacks update --stack-id $TASKS_STACK_ID -n tasks $TASK_FILES"
 influx apply --force true --stack-id $TASKS_STACK_ID -q
-
-
-INFLUX_TOKEN=$(influx auth list --json | jq -r '.[].token')
-INFLUX_ORG=$(influx org list --json | jq -r '.[].name')
-TELEGRAF_ID=$(influx telegrafs --json | jq -r '.[].id')
-TELEGRAF_URL="http://localhost:9999/api/v2/telegrafs/$TELEGRAF_ID"
--- start_telegraf.sh
+++ start_telegraf.sh
@@ -2,8 +2,25 @@
 
 export PATH=$GOPATH/src/github.com/influxdata/influxdb/bin/darwin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 
+CONFIG_PROFILE="default"
+if [ -z "$1" ]; then
+    echo "No env supplied, assuming local (default)"
+else
+    ENV_EXISTS=$(influx config set -n $1 -a | grep Error)
+    if [ -z "$ENV_EXISTS" ]; then
+        echo "Successfully set config profile to $1..."
+    else
+        echo "Config profile $1 not found. Exiting."
+        exit 1
+    fi
+    CONFIG_PROFILE=$1
+fi
+
+INFLUX_URL=$(influx config ls --json | jq -r ".$CONFIG_PROFILE.url")
+echo $INFLUX_URL
+
 INFLUX_TOKEN=$(influx auth list --json | jq -r '.[].token')
 INFLUX_ORG=$(influx org list --json | jq -r '.[].name')
 TELEGRAF_ID=$(influx telegrafs --json | jq -r '.[].id')
-TELEGRAF_URL="http://localhost:9999/api/v2/telegrafs/$TELEGRAF_ID"
+TELEGRAF_URL=$INFLUX_URL"/api/v2/telegrafs/$TELEGRAF_ID"
 INFLUX_TOKEN=$INFLUX_TOKEN INFLUX_ORG=$INFLUX_ORG telegraf --config $TELEGRAF_URL
